e856cc6aa482aaf9f2179de8b99a3d2463cb2a13
kobaken <kentafly88@gmail.com>
2023-05-06 16:14:35 +0900

    minil new

---
+++ README.md
@@ -0,0 +1,23 @@
+
+# NAME
+
+Acme::Onion - It's new $module
+
+# SYNOPSIS
+
+    use Acme::Onion;
+
+# DESCRIPTION
+
+Acme::Onion is ...
+
+# LICENSE
+
+Copyright (C) kobaken.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+kobaken <kentafly88@gmail.com>
---
+++ lib/Acme/Onion.pm
@@ -0,0 +1,39 @@
+package Acme::Onion;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Acme::Onion - It's new $module
+
+=head1 SYNOPSIS
+
+    use Acme::Onion;
+
+=head1 DESCRIPTION
+
+Acme::Onion is ...
+
+=head1 LICENSE
+
+Copyright (C) kobaken.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+kobaken E<lt>kentafly88@gmail.comE<gt>
+
+=cut
+
